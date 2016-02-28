dfaac335d7f80cb55bdfc0015040cf33da20fabb
Aaron Hill <aaronthill16@gmail.com>
2016-02-28 11:40:56 -0500

    Refactored code

--- ISBN.rb
+++ ISBN.rb
@@ -1,18 +1,10 @@
-def verify_length(number)
-
-	number.length == 10 or number.length == 13
-
-end
-
-def delete_hyphens_and_spaces(number)
+def isbn_check(number)
 
 	invalid_characters = ["-", " "]
 	invalid_characters.each do |character|
 		number.delete! character if number.include? character
-	
 	end
 	number
 
 end
-
 
--- ISBN_Tests.rb
+++ ISBN_Tests.rb
@@ -4,13 +4,14 @@ require_relative "ISBN.rb"
 class TestISBNNumber < Minitest::Test 
 
 	def test_length_is_10_or_13
-		assert_equal(true, verify_length("9780470059029"))
-		assert_equal(false, verify_length("12378"))
-		assert_equal(true, verify_length("9780470059"))
+		isbn_number = isbn_check("9780470059029")
+		assert_equal(true, isbn_number.length == 13 || isbn_number.length == 10)
+		isbn_number = isbn_check("978047005902")
+		assert_equal(false, isbn_number.length == 13 || isbn_number.length == 10)	
 	end
 
 	def test_remove_hyphens_and_spaces
-		assert_equal("9780470059029", delete_hyphens_and_spaces("978-047-00 5-9029"))
+		assert_equal("9780470059029", isbn_check("978-047-00 5-9029"))
 	end
 
 end
