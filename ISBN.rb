6d746eaf736541e9a3a2165dae85bfd90a77d0a0
Aaron Hill <aaronthill16@gmail.com>
2016-02-28 11:24:20 -0500

    Added second test

--- ISBN.rb
+++ ISBN.rb
@@ -1,6 +1,17 @@
 def verify_length(number)
 
-number.length == 10 or number.length == 13
+	number.length == 10 or number.length == 13
+
+end
+
+def delete_hyphens_and_spaces(number)
+
+	invalid_characters = ["-", " "]
+	invalid_characters.each do |character|
+		number.delete! character if number.include? character
+	
+	end
+	number
 
 end
 
--- ISBN_Tests.rb
+++ ISBN_Tests.rb
@@ -3,8 +3,14 @@ require_relative "ISBN.rb"
 
 class TestISBNNumber < Minitest::Test 
 
-	def test_number_of_elements_is_10_or_13
+	def test_length_is_10_or_13
 		assert_equal(true, verify_length("9780470059029"))
+		assert_equal(false, verify_length("12378"))
+		assert_equal(true, verify_length("9780470059"))
+	end
+
+	def test_remove_hyphens_and_spaces
+		assert_equal("9780470059029", delete_hyphens_and_spaces("978-047-00 5-9029"))
 	end
 
 end
