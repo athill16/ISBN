4caa49c008814e49bb601e7247f38747861004e9
Aaron Hill <aaronthill16@gmail.com>
2016-03-12 14:06:21 -0500

    Refactored code to make each function perform one task

--- ISBN_redo.rb
+++ ISBN_redo.rb
@@ -1,18 +1,19 @@
 def valid_isbn?(number)
+	isbn_without_invalid_characters = remove_invalid_characters(number)
+	isbn_without_x = remove_x_from_isbn_if_it_is_last_element(isbn_without_invalid_characters)
+	isbn_contain_all_digits = contain_all_digits?(isbn_without_x)
+	isbn10_sum = sum_of_isbn10_digits(isbn_without_invalid_characters)
+	isbn10_check = isbn10_check_digit_equal_sum?(isbn_without_invalid_characters)
+	isbn13_sum = sum_of_isbn13_digits(isbn_without_invalid_characters)
+	isbn13_check = isbn13_check_digit_equal_sum?(isbn_without_invalid_characters)
 
-	remove_invalid_characters(number)
-	invalid_characters(number)
-	isbn10_check_digit(number)
-	isbn13_check_digit(number)
-
-	if number.length == 10 && invalid_characters(number) == true  && isbn10_check_digit(number) == true
+	if isbn_without_invalid_characters.length == 10 && isbn_contain_all_digits == true && isbn10_check == true
 		true
-	elsif number.length == 13 && invalid_characters(number) == true && isbn13_check_digit(number) == true
+	elsif isbn_without_invalid_characters.length == 13 && isbn_contain_all_digits == true && isbn13_check == true
 		true
 	else
 		false
 	end
-
 end
 
 def remove_invalid_characters(number)
@@ -21,13 +22,27 @@ def remove_invalid_characters(number)
 	number
 end
 
-def invalid_characters(number)
+def contains_10_or_13_characters?(number)
 	remove_invalid_characters(number)
-	number = number.split("")
-# Removes x from the ISBN if it's in the check digit position, since X is an invalid character unless it is at the end of an ISBN-10.
-	if number.length == 10 && number[9] == "X" || number[9] == "x"
-		number.slice!(9)
+	if number.length == 10 || number.length == 13
+		true
+	else 
+		false
 	end
+end
+
+def remove_x_from_isbn_if_it_is_last_element(number)
+	array = number.split("")
+# Removes x from the ISBN if it's in the check digit position, since X is an invalid character unless it is at the end of an ISBN-10.
+	if number.length == 10 && array[9] == "X" || array[9] == "x"
+		number.slice!(9)
+		number
+	else
+		number
+	end
+end
+
+def contain_all_digits?(number)
 # Checks if all elements in ISBN are numbers and not letters/characters.
 	if number =~ /\D/
 		false
@@ -36,44 +51,51 @@ def invalid_characters(number)
 	end
 end
 
-def isbn10_check_digit(number)
-	remove_invalid_characters(number)
-	number = number.split("")
+def sum_of_isbn10_digits(number)
+	array = number.split("")
 	sum = 0
-	number.each_with_index do |value, position|
+	array.each_with_index do |value, position|
 # Multiplies each value by its index plus one. So the first number multiples by 1 instead of 0.
-
 		break if position == 9
 		sum += value.to_i * (position + 1)
 	end
 	sum = sum % 11
-	if sum == 10 && number[9] == "X" || number[9] == "x"
+end
+
+def isbn10_check_digit_equal_sum?(number)
+	array = number.split("")
+	sum = sum_of_isbn10_digits(number)
+	if sum == 10 && array[9] == "X" || array[9] == "x"
 		true
-	elsif number[9].to_i == sum
+	elsif array[9].to_i == sum
 		true
 	else
 		false
 	end
 end
 
-def isbn13_check_digit(number)
-	remove_invalid_characters(number)
-	number = number.split("")
+def sum_of_isbn13_digits(number)
+	array = number.split("")
 	sum = 0
-	number.each_with_index do |value, position|
+	array.each_with_index do |value, position|
 # Multiples 1 by numbers with even indexes and multiplies 3 by numbers with odd indexes.
 		break if position == 12
 		if position % 2 == 0
 			sum += value.to_i
 		else 
-			sum+= value.to_i * 3
+			sum += value.to_i * 3
 		end
 	end
+	sum
+end
+
+def isbn13_check_digit_equal_sum?(number)
+	array = number.split("")
+	sum = sum_of_isbn13_digits(number)
 	sum = sum % 10
 	sum = 10 - sum
 	sum = sum % 10
 	number[12].to_i == sum
 end
-
 
 
--- ISBN_redo_tests.rb
+++ ISBN_redo_tests.rb
@@ -4,33 +4,37 @@ require_relative "ISBN_redo.rb"
 class TestISBN < Minitest::Test
 
 	def test_ISBN_has_10_or_13_numbers
-		assert_equal(true, valid_isbn?("1232455369"))
-		assert_equal(true, valid_isbn?("4692874753241"))
+		assert_equal(true, contains_10_or_13_characters?("1232455369"))
+		assert_equal(true, contains_10_or_13_characters?("4692874753241"))
 	end
 
-	def test_ISBN_with_spaces_and_hyphens_returns_true
-		assert_equal(true, valid_isbn?("1 2324 55369"))
-		assert_equal(true, valid_isbn?("469287475 324 1"))
+	def test_ISBN_with_spaces_and_hyphens_returns_valid_format
+		assert_equal("1232455369", remove_invalid_characters("1 2324 55369"))
+		assert_equal("4692874753241", remove_invalid_characters("469287475 324 1"))
 	end
 
 	def test_invalid_character_returns_false
-		assert_equal(false, valid_isbn?("12324X5369"))
+		assert_equal(false, contain_all_digits?("12324X5369"))
 	end
 
-	def test_isbn10_that_ends_with_X_is_valid
-		assert_equal(true, valid_isbn?("877195869X"))
+	def test_sum_of_ISBN10_is_correct
+		assert_equal(3, sum_of_isbn10_digits("123"))
 	end
 
-	def test_ISBN10_check_digit
-		assert_equal(true, isbn10_check_digit("1 2324 55369"))
+	def test_sum_of_ISBN13_is_correct
+		assert_equal(8, sum_of_isbn13_digits("121"))
 	end
 
-	def test_ISBN13_check_digit
-		assert_equal(true, isbn13_check_digit("469287475 324 1"))
+	def test_isbn10_is_valid
+		assert_equal(true, valid_isbn?("0471958697"))
 	end
 
-	def test_ISBN10_with_X_in_middle_and_end_is_invalid
-		assert_equal(false, valid_isbn?("8771X5869X"))
+	def test_isbn13_is_valid
+		assert_equal(true, valid_isbn?("9780471486480"))
 	end
 
 end
+
+
+
+
