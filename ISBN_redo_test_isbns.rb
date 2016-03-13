ff2fd40ca2e7cdfde5874bdea54b60bf66c51e01
Aaron Hill <aaronthill16@gmail.com>
2016-03-13 15:29:15 -0400

    Generate new file showing whether ISBN's are valid or invalid

--- ISBN_redo_test_isbns.rb
+++ ISBN_redo_test_isbns.rb
@@ -1,18 +1,23 @@
 require_relative "ISBN_redo.rb"
 require 'csv'
 
-isbn_numbers = CSV.read('isbn_numbers.rb')
+f = File.open('isbn_validity_output.rb', 'w')
 
-def test_validity_of_isbns()
+def test_validity_of_isbns(f)
 	CSV.foreach('isbn_numbers.rb') do |item, number|
 		validity = valid_isbn?(number)
 		if validity == true
-			puts "#{number}, valid"
+			f << "#{number}, valid\n"
 		else 
-			puts "#{number}, invalid"
+			f << "#{number}, invalid\n"
 		end
 	end
 end
+
+test_validity_of_isbns(f)
+
+f.close
+
 
 
 
