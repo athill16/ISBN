d5fc5b5c8b08bcc29db3721ae4c78bb3d8514c5a
Aaron Hill <aaronthill16@gmail.com>
2016-03-19 12:16:20 -0400

    Deleted unneeded function

--- ISBN_redo_test_isbns.rb
+++ ISBN_redo_test_isbns.rb
@@ -4,7 +4,6 @@ require 'csv'
 f = File.open('isbn_validity_output.csv', 'w')
 f << "Item,  ISBN,     Validity\n"
 
-def test_validity_of_isbns(f)
 	CSV.foreach('isbn_numbers.csv') do |item, number|
 		validity = valid_isbn?(number)
 		if validity == true
@@ -13,9 +12,6 @@ def test_validity_of_isbns(f)
 			f << "#{item}, #{number}, invalid\n"
 		end
 	end
-end
-
-test_validity_of_isbns(f)
 
 f.close
 
