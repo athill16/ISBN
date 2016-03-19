7ce09604d4a55946c7d159e87c2049fb4daebc58
Aaron Hill <aaronthill16@gmail.com>
2016-03-19 12:06:23 -0400

    Added Item column

--- ISBN_redo_test_isbns.rb
+++ ISBN_redo_test_isbns.rb
@@ -2,15 +2,15 @@ require_relative "ISBN_redo.rb"
 require 'csv'
 
 f = File.open('isbn_validity_output.csv', 'w')
-f << "ISBN,       Validity\n"
+f << "Item,  ISBN,     Validity\n"
 
 def test_validity_of_isbns(f)
 	CSV.foreach('isbn_numbers.csv') do |item, number|
 		validity = valid_isbn?(number)
 		if validity == true
-			f << "#{number}, valid\n"
+			f << "#{item}, #{number}, valid\n"
 		else 
-			f << "#{number}, invalid\n"
+			f << "#{item}, #{number}, invalid\n"
 		end
 	end
 end
